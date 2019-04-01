82b9589bfbb6dc8e59e887060c6a2f466534b14f
Wes Byrne <wesburn@me.com>
2019-04-01 10:38:37 -0700

    Now sends a notification to the reloaded plugin, optionally supplying data to it.

--- README.md
+++ README.md
@@ -9,7 +9,45 @@ In cases where you would need to restart Sketch to refresh your plugin, Plugin R
 1. Install the plugin
 2. Select setup (reload will also work the first time)
 3. Enter the identifier of the plugin you are developing and want to reload.
+4. If desired, add a query string to send to your plugin. More info in [Query Strings & Handling Reloads](#handling-reloads)
 4. Select reload
+
+## Query Strings & Handling Reloads
+<a id="handling-reloads"></a<>
+
+As part of the setup, you can optionally provide a query string to send data to your plugin after it is reloaded. This will be delivered via a handler registered for the `HandleURL` action.
+
+An example maniftest would look like:
+```js
+{
+    ... other manifest info
+    "commands": [
+        {
+            "name": "Actions",
+            "identifier": "action-handlers",
+            "script": "./index.js",
+            "handlers": {
+                "actions": {
+                    "HandleURL": "handleURL"
+                }
+            }
+        }
+    ]
+}
+```
+
+Then in your handler
+```js
+export function handleURL(context) {
+    const query = context.actionContext.query;
+    const reload = Number(query.reload);
+    console.log(reload);
+    if (reload === 1) { // `reload=1` is always included in the query
+        // Do something after reloader reloads your plugin
+    }
+}
+```
+
 
 ## Thanks
 
--- src/commands.js
+++ src/commands.js
@@ -1,18 +1,63 @@
-import sketch from 'sketch'
+import sketch from "sketch";
 // documentation: https://developer.sketchapp.com/reference/api/
 
 export const setup = function() {
-  sketch.UI.getInputFromUser("Which plugin would you like to reload?", {
-    initialValue: sketch.Settings.settingForKey('targetPluginIdentifier')
-  }, (err, value) => {
-    if (value) {
-      sketch.Settings.setSettingForKey('targetPluginIdentifier', value)
-      reload()
+  sketch.UI.getInputFromUser(
+    "(1/2) Which plugin would you like to reload?",
+    {
+      initialValue: sketch.Settings.settingForKey("targetPluginIdentifier")
+    },
+    (err, value) => {
+      if (value) {
+        sketch.Settings.setSettingForKey("targetPluginIdentifier", value);
+        promptForCommand();
+      }
     }
-  })
+  );
+};
+
+function promptForCommand() {
+  sketch.UI.getInputFromUser(
+    "(2/2) Send data to your plugin after reloading.",
+    {
+      initialValue: sketch.Settings.settingForKey("targetPluginQuery") || "",
+      description:
+        "Provide a query string to send data to the plugin after reloading. ex. `dance=moonwalk&skill=high`.\n\nNote, you will need to add a handler to your plugin to use this."
+    },
+    (err, value) => {
+      sketch.Settings.setSettingForKey("targetPluginQuery", null);
+      if (value) {
+        sketch.UI.alert(
+          "Setup Complete",
+          "Note you will need to setup a handler in your plugin to use this extra data.\n\nSee help in Plugins > Plugin Reloader > Help for more info."
+        );
+      }
+      reload();
+    }
+  );
 }
+
+function appURL() {
+  const app = String(MSArchiveHeader.metadataForNewHeader()["app"]);
+  switch (app) {
+    case "com.bohemiancoding.sketch3":
+      return "sketch://plugin";
+    case "com.bohemiancoding.sketch3.beta":
+      return "sketch-beta://plugin";
+    case "com.bohemiancoding.sketch3.private":
+      return "sketch-private://plugin";
+    default:
+      throw new Error("Unexpected sketch app");
+  }
+}
+
+function openURL(urlString) {
+  let url = NSURL.alloc().initWithString(urlString);
+  NSWorkspace.sharedWorkspace().openURL(url);
+}
+
 export const reload = function() {
-  const identifier =  sketch.Settings.settingForKey('targetPluginIdentifier')
+  const identifier = sketch.Settings.settingForKey("targetPluginIdentifier");
   const pm = AppController.sharedInstance().pluginManager();
   const plugin = pm.plugins()[identifier];
   if (plugin) {
@@ -20,11 +65,17 @@ export const reload = function() {
     pm.reloadPlugins();
     pm.enablePlugin_(plugin);
     pm.reloadPlugins();
+
+    const query = sketch.Settings.settingForKey("targetPluginQuery");
+    let urlString = `${appURL()}/${identifier}?reload=1&${query}`;
+    openURL(urlString);
+
+    sketch.UI.message(`🔄 Plugin Reloaded - ${identifier}`);
   } else {
-    setup()
+    setup();
   }
+};
 
-
-
-}
-
+export const openHelp = function() {
+  openURL("https://github.com/TheNounProject/SketchPluginReloader");
+};
